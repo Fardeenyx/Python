25f25b93ac0c7537b529aa138e7aee4d7767a103
FARDEEN <firdoshshaikh771@gmail.com>
2026-02-26 15:26:38 +0530

    Set-functions-stage-1

---
+++ SET.py
@@ -0,0 +1,15 @@
+# Set = {} unordered and immutable, but Add/Remove OK. No duplicates
+
+fruits = {"apple", "orange", "banana", "coconut"}
+
+# print(fruits)
+
+# print(len(fruits))
+
+# fruits.add("Pineapple")
+
+# fruits.remove("Apple")
+
+# fruits.pop()
+
+# fruits.clear()
