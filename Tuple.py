e88db2bd9a9e2077a33e676a7e78a982f90acc21
FARDEEN <firdoshshaikh771@gmail.com>
2026-02-26 15:41:31 +0530

    Tuple-function-stage-1

---
+++ Tuple.py
@@ -0,0 +1,14 @@
+# Tuple = () orderred and unchangeable. Duplicates Ok. FASTER
+
+fruits = ("apple", "orange", "banana", "coconut")
+
+# print(len(fruits))
+
+# print("Pineapple" in fruits)
+
+# fruits.index("apple")
+
+# fruits.count("coconut")
+
+# for fruit in fruits:
+#   print(fruit)
